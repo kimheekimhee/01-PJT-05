79477aac09a97aec3370d3c79475541578b99250
chul1631 <sonky201@gmail.com>
2022-08-12 17:23:28 +0900

    1기_1회차_손세철_알고리즘_모의고사

--- 1회차/손세철/3_Flatten.py
+++ 1회차/손세철/3_Flatten.py
@@ -1,3 +1,15 @@
 import sys
 
 sys.stdin = open("_Flatten.txt")
+
+for i in range(1,11):
+    dump = int(input())
+
+    box = list(map(int, input().split()))
+
+    while dump:
+        max_box = max(box)
+        min_box = 
+############################################################
+
+    print('#{} {}'.format())
