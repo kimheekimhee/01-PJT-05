84f4595d19a55b564cac9766991edbf2a6925335
an dong woo <akskwha@naver.com>
2022-08-12 17:28:42 +0900

    20220812모의고사

--- 1회차/안동우/0_반반.py
+++ 1회차/안동우/0_반반.py
@@ -1,3 +1,22 @@
+from calendar import c
 import sys
 
+
 sys.stdin = open("_반반.txt")
+
+TC= int(input())
+for z in range(1,TC+1):
+    s=list(input())
+
+    a=0
+    for q in s:
+        if s.count(q) == 2:
+            pass
+        else:
+            a=1
+            break
+    if  a == 1:
+        print(f'#{z}','No')
+ 
+    else:
+        print(f'#{z}','Yes')
