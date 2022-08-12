59650d53e3212f3246e8b1984fef9623197ba022
woocheolll <woocheol2868@gmail.com>
2022-08-12 17:22:41 +0900

    모의고사

--- 1회차/신우철/1_모음이보이지않는사람.py
+++ 1회차/신우철/1_모음이보이지않는사람.py
@@ -1,3 +1,12 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+t = int(input())
+for test in range(1,t+1):
+    text = str(input())
+    text = text.replace('a','')
+    text = text.replace('e','')
+    text = text.replace('i','')
+    text = text.replace('o','')
+    text = text.replace('u','')
+    print(f'#{test}' ,text)
