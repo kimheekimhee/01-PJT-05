f05b87d261a43a94ff17a96299ce3024730f5aaa
dortkthf <dortkthf@gmail.com>
2022-08-12 17:21:14 +0900

    마지막 문제 제외후 모두 해결

--- 2회차/이준엽/2_퍼펙트셔플.py
+++ 2회차/이준엽/2_퍼펙트셔플.py
@@ -1,3 +1,24 @@
-import sys
+# import sys
 
-sys.stdin = open("_퍼펙트셔플.txt")
+# sys.stdin = open("_퍼펙트셔플.txt")
+
+t = int(input())
+for i in range(1,t+1):
+    n = int(input())
+    cards = list(input().split())
+    newcards = []
+    if n%2 == 0:
+        first_cards = cards[:int(n/2)]
+        second_cards = cards[int(n/2):]
+        for _ in range(int(n/2)):
+            newcards.append(first_cards[_])
+            newcards.append(second_cards[_])
+    else :
+        first_cards = cards[:(n//2)+1]
+        seconds_cards = cards[(n//2)+1:]
+        newcards.append(first_cards[0])
+        for _ in range(n//2):
+            newcards.append(seconds_cards[_])
+            newcards.append(first_cards[_+1])
+    print(f"#{i}",*newcards)
+
--- 2회차/이준엽/4_창용마을무리의개수.py
+++ 2회차/이준엽/4_창용마을무리의개수.py
@@ -1,3 +1,29 @@
 import sys
 
 sys.stdin = open("_창용마을무리의개수.txt")
+
+t = int(input())
+for _ in range(1,t+1):
+    n,m = map(int,input().split())
+    relation = [[] for i in range(n+1)]
+    visited = [False for i in range(n+1)]
+    stack = []
+    cnt = 0
+    for i in range(m):
+        a,b = map(int,input().split())
+        relation[a].append(b)
+        relation[b].append(a)
+
+
+    for j in range(1,n+1):
+        if visited[j] == False:
+            stack.append(j)
+            visited[j] = True 
+            while stack:
+                start = stack.pop()  
+                for num in relation[start]:
+                    if visited[num] == False:
+                        stack.append(num)
+                        visited[num] = True
+            cnt+=1
+    print(f'#{_} {cnt}')
