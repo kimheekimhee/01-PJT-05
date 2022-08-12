b5beeeb5953c73d38bcaf0f95b10445aed67b3e4
jsy78 <whtjd1541@gmail.com>
2022-08-12 19:01:41 +0900

    5번까지 해결, 주석 및 후기 추가

--- 3회차/조성윤/5_등산로조성.py
+++ 3회차/조성윤/5_등산로조성.py
@@ -1,72 +1,45 @@
-from pprint import pprint
 import sys
 
 sys.stdin = open("_등산로조성.txt")
 
 delta = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
-
-def DFS_construction(r, c, h, limit, length, n, k, area, visited) :
+def DFS_construction(r, c, limit, length) : # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
     global max_length
-    visited[r][c] = 1 # 방문
 
-    if max_length < length :
-        max_length = length
-    
     for dr, dc in delta :
-        nr, nc = r + dr, c + dc # 다음 위치
-        if 0 <= nr < n and 0 <= nc < n : # 범위 체크
-            nh = area[nr][nc] # 다음 위치의 높이
-            if visited[nr][nc] == 0 : # 다음 위치의 방문 여부 체크
-                if nh < h : # 다음 위치의 원래 높이가 기존 위치의 높이보다 낮음 
-                    DFS_construction(nr, nc, nh, 1, length+1, n, k, area, visited) # 다음 위치 좌표 및 높이, 공사 안 함, 길이 증가
-                elif nh >= h and limit != 0 : # 다음 위치의 원래 높이가 기존 위치보다 높거나 같고 공사 아직 안 함
-                    for i in range(1, k+1) :
-                        nh -= i # 다음 위치의 높이를 정수 단위로 깎아나감
-                        if nh < h : # 깎은 높이가 기존 높이보다 낮아지면
-                            break # 반복문 탈출
-                    DFS_construction(nr, nc, nh, 0, length+1, n, k, area, visited) # 다음 위치 좌표 및 높이, 공사 안 함, 길이 증가   
-    
-    visited[r][c] = 0 # 끝자락에 도달했으면 백트래킹하면서 방문 해제
-
-    # stack = list()
-    # stack.append((r, c, area[r][c], 1, 1)) # 처음 좌표 (r, c), 처음 좌표 높이, 남은 공사 횟수, 현재 등산로 길이
-    # while stack :
-    #     pr, pc, ph, plimit, plength = stack.pop()
-    #     if visited_list[pr][pc] == 0 : # 방문 여부 체크
-    #         visited_list[pr][pc] = 1
-    #     for dr, dc in delta :
-    #         nr, nc = pr + dr, pc + dc # 다음 위치
-    #         if 0 <= nr < n and 0 <= nc < n : # 범위 체크
-    #             nh = area[nr][nc] # 다음 위치의 높이
-    #             if visited_list[nr][nc] == 0 : # 다음 위치의 방문 여부 체크
-    #                 if nh < ph : # 다음 위치의 원래 높이가 기존 위치의 높이보다 낮음 
-    #                     stack.append((nr, nc, nh, plimit, plength+1)) # 다음 위치 좌표, 공사 안 함, 길이 증가
-    #                 elif nh >= ph and plimit != 0 : # 다음 위치의 원래 높이가 기존 위치보다 높거나 같고 공사 아직 안 함
-    #                     for i in range(1, k+1) :
-    #                         nh -= i # 다음 위치의 높이를 정수 단위로 깎아나감
-    #                         if nh < ph : # 깎은 높이가 기존 높이보다 낮아지면
-    #                             stack.append((nr, nc, nh, plimit-1, plength+1)) # 깎았던 다음 위치 좌표, 공사했음, 길이 증가
-        
-    #     # print(stack)
-    #     # print(pr, pc, ph, plimit, plength)
-    #     # pprint(visited_list)
-    #     if max_length < plength : # 가장 긴 길이 갱신
-    #         max_length = plength  
-
-    # return max_length
-                    
+        nr, nc = r + dr, c + dc # 다음 위치 좌표
+        if not (0 <= nr < N and 0 <= nc < N) : # 범위 체크
+            continue
+        if visited[nr][nc] == 1 : # 다음 위치의 방문 여부 체크
+            continue
+        if mountain[nr][nc] < mountain[r][c] : # 다음 위치의 원래 높이가 기존 위치의 높이보다 낮음 
+            visited[nr][nc] = 1 # 방문
+            DFS_construction(nr, nc, limit, length+1) # 다음 위치 좌표 및 높이, 공사 안 함, 길이 증가
+            visited[nr][nc] = 0 # 끝자락에 도달했으면 역순으로 되돌아가면서 방문 해제
+        elif mountain[nr][nc] >= mountain[r][c] and limit > 0 : # 다음 위치의 원래 높이가 기존 위치보다 높거나 같고 공사 가능 횟수 남음
+            for i in range(1, K+1) :
+                mountain[nr][nc] -= i # 다음 위치의 높이를 정수 단위로 최대 K만큼 깎음
+                if mountain[nr][nc] < mountain[r][c] : # 깎은 높이가 기존 높이보다 낮아지면
+                    visited[nr][nc] = 1 # 방문
+                    DFS_construction(nr, nc, limit-1, length+1) # 다음 위치 좌표 및 높이, 공사함, 길이 증가   
+                    visited[nr][nc] = 0 # 끝자락에 도달했으면 역순으로 되돌아가면서 방문 해제
+                mountain[nr][nc] += i # 다음 위치의 높이 복구
+    max_length = max(max_length, length) # 길이 갱신
 
 T = int(input())
 for test_case in range(1, T+1) :
     N, K = map(int, input().split())
     mountain = [list(map(int, input().split())) for _ in range(N)]
-    visited_list = [[0 for _ in range(N)] for _ in range(N)]
+    visited = [[0 for _ in range(N)] for _ in range(N)]
     high_len = max(map(max, mountain))
     max_length = 0
+
     for i in range(N) :
         for j in range(N) :
-            if mountain[i][j] == high_len :
-                DFS_construction(i, j, mountain[i][j], 1, 1, N, K, mountain, visited_list)
+            if mountain[i][j] == high_len :           
+                visited[i][j] = 1 # 맨 처음 위치 방문
+                DFS_construction(i, j, 1, 1) # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
+                visited[i][j] = 0 # 모든 호출이 끝나고 마지막으로 처음 위치 방문 해제
 
-    print(max_length)
+    print(f'#{test_case} {max_length}')
--- 3회차/조성윤/README.md
+++ 3회차/조성윤/README.md
@@ -1,1 +1,78 @@
 ## 후기 작성
+마지막 알고리즘 수업이라니 아쉽다...
+### 반반
+문자열의 각 문자의 count가 전부 2면 조건을 만족한다.
+### 모음이 보이지 않는 사람
+그냥 replace 5번... 파이썬 갓갓
+### 퍼펙트 셔플
+카드 수가 홀수일 때와 짝수일 때를 나눠서 풀었다. 
+슬라이싱을 통해 카드 덱을 반으로 나눠서 순서대로 추가해줬다.
+### Flatten
+리스트의 최댓값을 1 빼고 최솟값을 1 더한다. 
+이걸 주어진 덤프 횟수만큼 반복한다.
+### 창용 마을 무리의 개수
+[백준 11724 연결 요소의 개수](https://www.acmicpc.net/problem/11724)와 동일한 문제였다.
+### 등산로 조성
+스택을 사용한 반복문으로 풀려고 시도했으나
+visit 백트래킹 과정에 막혀서 결국 익숙하지도 않은 재귀를 시도해야만 했다.
+```python
+def DFS_construction(r, c, limit, length) : # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
+    global max_length
+    visited[r][c] = 1
+    for dr, dc in delta :
+        .
+        .
+        if mountain[nr][nc] < mountain[r][c] : # 다음 위치의 원래 높이가 기존 위치의 높이보다 낮음
+            DFS_construction(nr, nc, limit, length+1) # 다음 위치 좌표 및 높이, 공사 안 함, 길이 증가
+        .
+        .
+        .
+    max_length = max(max_length, length) # 길이 갱신
+    visited[r][c] = 0
+
+T = int(input())
+for test_case in range(1, T+1) :
+    .
+    .
+    .
+    for i in range(N) :
+        for j in range(N) :
+            if mountain[i][j] == high_len :           
+                DFS_construction(i, j, 1, 1) # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
+    .            
+    .            
+```
+처음엔 이렇게 함수의 처음과 끝부분에서 visit 백트래킹을 시도했고 정답인 줄 알았는데 아니었다.
+```python
+def DFS_construction(r, c, limit, length) : # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
+    .
+    .
+    .
+            visited[nr][nc] = 1 # 방문
+            DFS_construction(nr, nc, limit, length+1) # 다음 위치 좌표 및 높이, 공사 안 함, 길이 증가
+            visited[nr][nc] = 0 # 끝자락에 도달했으면 역순으로 되돌아가면서 방문 해제
+       .
+       .
+       .
+                if mountain[nr][nc] < mountain[r][c] : # 깎은 높이가 기존 높이보다 낮아지면
+                    visited[nr][nc] = 1 # 방문
+                    DFS_construction(nr, nc, limit-1, length+1) # 다음 위치 좌표 및 높이, 공사함, 길이 증가   
+                    visited[nr][nc] = 0 # 끝자락에 도달했으면 역순으로 되돌아가면서 방문 해제
+                
+                .
+                .
+T = int(input())
+for test_case in range(1, T+1) :
+    .
+    .
+    .
+    for i in range(N) :
+        for j in range(N) :
+            if mountain[i][j] == high_len :           
+                visited[i][j] = 1 # 맨 처음 위치 방문
+                DFS_construction(i, j, 1, 1) # 현재 위치 좌표, 공사 가능 횟수, 현재 길이
+                visited[i][j] = 0 # 모든 호출이 끝나고 마지막으로 처음 위치 방문 해제
+    .
+    .
+```
+이렇게 수정했더니 정답이 나오긴 했는데 아직 잘 모르겠다...
