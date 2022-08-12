04ccb6bcd486f6d08296355d8011437f6c793266
hady1024 <hady1024@gmail.com>
2022-08-12 17:55:26 +0900

    220812 모의고사

--- 2회차/하두연/1_모음이보이지않는사람.py
+++ 2회차/하두연/1_모음이보이지않는사람.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    word = input()
+    answer = ''
+
+    for i in word:
+        if i not in ['a', 'e', 'i', 'o', 'u']:
+            answer += i
+
+    print(f"#{test_case} {answer}")
