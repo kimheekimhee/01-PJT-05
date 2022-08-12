33dc250afdf0b22704827f56b784b27d8b0ed68b
hjs721 <dse04145@gmail.com>
2022-08-12 17:17:04 +0900

    3회차 현지수 모의고사

--- 3회차/현지수/2_퍼펙트셔플.py
+++ 3회차/현지수/2_퍼펙트셔플.py
@@ -1,3 +1,43 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+t = int(input())
+for test_case in range(1, t + 1):
+    n = int(input())
+    cards = list(input().split())
+    # 첫번째 카드뭉치
+    fir = []
+    # 두번째 카드뭉치
+    sec = []
+    # 카드수가 홀수인지 짝수인지에 따라 카드뭉치 구분
+    # 짝수라면
+    if len(cards) % 2 == 0:
+        for i in range(len(cards)):
+            if i < len(cards) // 2:
+                # 중앙값 포함x(없음)
+                fir.append(cards[i])
+            else:
+                sec.append(cards[i])
+    # 홀수라면
+    if len(cards) % 2 == 1:
+        for i in range(len(cards)):
+            # 중앙값 포함
+            if i <= len(cards) // 2:
+                fir.append(cards[i])
+            else:
+                sec.append(cards[i])
+    
+    # 결과 리스트
+    result = []
+    # 일단 홀짝 구분 없이 마지막 카드 빼고 공통적으로 번갈아가면서 하나씩 넣어줌
+    for k in range(len(fir) - 1):
+        result.append(fir[k])
+        result.append(sec[k])
+    # 첫번째 카드뭉치 마지막 카드 넣어줌
+    result.append(fir[-1])
+    # 홀수면 끝이겠지만 짝수면 두번째 카드뭉치에 마지막 카드 한장 남아있음
+    if len(fir) == len(sec):
+        # 마지막 카드 추가
+        result.append(sec[-1])
+    print(f'#{test_case}', *result)
--- 3회차/현지수/5_등산로조성.py
+++ 3회차/현지수/5_등산로조성.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_등산로조성.txt")
+
+t = int(input())
+for test_case in range(1, t + 1):
+    n, k = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+
+    # 4방향 델타탐색
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
+
+    # 이게몬가요...
