dcbf1f0610adc2a490dde8c74ef9c3f3775d224e
han-gaeul <gmlakdcjs314@gmail.com>
2022-08-12 17:15:39 +0900

    2회차 한가을 풀이

--- 2회차/한가을/0_반반.py
+++ 2회차/한가을/0_반반.py
@@ -1,3 +1,35 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+# 길이 4의 알파벳 대문자로 이루어진 문자열 S가 주어졌을 때
+# S에 정확히 두 개의 서로 다른 문자가 등장하고
+# 각 문자가 정확히 두 번 등장하는지 판별
+
+# 첫 번째 줄에 테스트 케이스의 수 TC가 주어진다
+# 이후 TC개의 테스트 케이스가 새 줄로 주어진다
+# 첫 번째 줄에 문자열 S가 주어진다
+
+# 각 테스트 케이스마다 조건이 만족되면 Yes 아니면 No 출력
+
+TC = int(input())
+
+for k in range(1, TC + 1):
+    S = list(input())
+
+    result = 0
+
+    for i in range(4):
+        count = 0
+
+        for j in range(4):
+            if S[i] == S[j]:
+                count += 1
+        
+        if count == 2:
+            result += 1
+    
+    if result == 4:
+        print('#{} {}'.format(k, 'Yes'))
+    else:
+        print('#{} {}'.format(k, 'No'))
--- 2회차/한가을/2_퍼펙트셔플.py
+++ 2회차/한가을/2_퍼펙트셔플.py
@@ -1,3 +1,43 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+# 카드를 퍼펙트 셔플 한다는 것은 카드 덱을 정확히 절반으로 나누고
+# 나눈 것들에서 교대로 카드를 뽑아 새로운 덱을 만드는 것을 의미
+# N개의 카드가 있는 덱이 주어질 때
+# 이를 퍼펙트 셔플하면 어떤 순서가 되는지 출력
+# 만약 N이 홀수이면 교대로 놓을 때
+# 먼저 놓는 쪽에 한 장이 더 들어가게 하면 된다
+
+# 첫 번째 줄에 테스트 케이스의 수 T가 주어진다
+# 각 테스트 케이스의 첫 번째 줄에는 자연수 N이 주어진다
+# 두 번째 줄에는 덱에 카드가 놓인 순서대로
+# N개의 카드 이름이 공백으로 구분되어 주어진다
+# 카드의 이름은 알파벳 대문자와 '-'만으로 이루어짐
+
+# 각 테스트 케이스마다 주어진 덱을 퍼펙트 셔플한 결과를
+# 한 줄에 카드 이름을 공백으로 구분하여 출력
+
+# 출력 예시
+#1 A D B E C F
+#2 JACK KING QUEEN ACE
+#3 ALAKIR LORD-JARAXXUS ALEXSTRASZA AVIANA DR-BOOM
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    cards = list(input().split())
+    cardList = []
+    l = 0
+    r = (N+1) // 2
+
+    for _ in range(N//2):
+        cardList.append(cards[l])
+        cardList.append(cards[r])
+        l, r = l + 1, r + 1
+
+    if N % 2:
+        cardList.append(cards[N//2])
+
+    print('#{} {}'.format(tc, ' '.join(map(str, cardList))))
