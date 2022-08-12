740ca21414ab2fc653fc7f8ef6e3899de158fbf3
1c0332zz <1c0332zz@gmail.com>
2022-08-12 17:22:34 +0900

    1회차 송강섭

--- 1회차/송강섭/1_모음이보이지않는사람.py
+++ 1회차/송강섭/1_모음이보이지않는사람.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+for tc in range(1, T+1):
+
+    word = list(input())
+    aeiou = ['a', 'e', 'i', 'o', 'u']
+    arr = []
+
+    for j in word:
+        if j not in ['a', 'e', 'i', 'o', 'u']:
+            arr.append(j)
+            
+    print(f'#{tc}', end=' ') 
+    print(*arr, sep=(''))
