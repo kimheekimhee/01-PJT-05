ecb7d887eadca30bb66814aedc65570748f89a05
runedemonic <runedemonic@naver.com>
2022-08-12 16:46:20 +0900

    20220812 test complete

--- 3회차/윤효근/0_반반.py
+++ 3회차/윤효근/0_반반.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+T = int(input())
+for test_case in range(T):
+    count = 0
+    s = input()
+    result=[]
+    for i in s:
+        result.append(s.count(i))
+    for i in result:
+        if i == 2:
+            count += 1
+    if count == 4:
+        print(f'#{test_case +1} Yes')
+    else:
+        print(f'#{test_case+ 1} No')
--- 3회차/윤효근/3_Flatten.py
+++ 3회차/윤효근/3_Flatten.py
@@ -1,3 +1,18 @@
 import sys
 
 sys.stdin = open("_Flatten.txt")
+for test_case in range(1,11):
+    limit = int(input())
+    h = list(map(int,input().split()))
+    h.sort()
+
+    aver = sum(h)//len(h)
+    while True:
+        h[h.index(max(h))] -= 1
+        h[h.index(min(h))] += 1
+        limit -= 1
+        if limit == 0:
+            break
+
+    print(f'#{test_case} {(max(h)-min(h))}')
+
