b47585f7d34f2cfadc5b2c7c4c201f207277fac8
slugjeon <youngman0515@naver.com>
2022-08-12 16:15:36 +0900

    08/12

--- 2회차/전준영/2_퍼펙트셔플.py
+++ 2회차/전준영/2_퍼펙트셔플.py
@@ -1,3 +1,35 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+T=int(input())
+dic=['a','e','i','o','u']
+answer=[]
+for _ in range(T):
+    length=int(input())
+    arr=input().split()
+    if(length%2==0):
+        arr0=arr[:length//2]
+        arr1=arr[length//2:]
+        arr=[]
+        for i in range(length):
+            if(i%2==0):
+                arr.append(arr0[i//2])
+            else:
+                arr.append(arr1[i//2])
+    else:
+        arr0=arr[:length//2+1]
+        arr1=arr[length//2+1:]
+        arr=[]
+        for i in range(length):
+            if(i%2==0):
+                arr.append(arr0[i//2])
+            else:
+                arr.append(arr1[i//2])
+    answer.append(arr)
+cnt=1
+for m in answer:
+    print("#"+str(cnt),end=' ')
+    for k in m:
+       print(k,end=' ')
+    print()
+    cnt+=1
