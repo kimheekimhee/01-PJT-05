4ec3db6628d10db59bb2edbc171e3a11d0d4128f
na_hyeong <skgud9511@gmail.com>
2022-08-12 17:24:16 +0900

    2회차_김나형_PJT_05

--- 2회차/김나형/4_창용마을무리의개수.py
+++ 2회차/김나형/4_창용마을무리의개수.py
@@ -1,3 +1,30 @@
+from re import L
 import sys
 
 sys.stdin = open("_창용마을무리의개수.txt")
+
+T = int(input())
+for tc in range(1,T + 1):
+    N, M = map(int, input().split())
+
+    graph = [[] for _ in range(N + 1)]
+    visited = [0] * (N + 1)
+
+    for _ in range(M):
+            x, y = map(int, input().split())
+            graph[x].append(y)
+            graph[y].append(x)
+
+    def dfs(v):
+        for j in graph[v]:
+            visited[v] = 1
+            if visited[j] == 0:
+                dfs(j)
+
+    cnt = 0
+    for k in range(1, N+1):
+        if visited[k] == 0:
+            cnt += 1
+            dfs(k)
+
+    print(f'#{tc} {cnt}')
--- 2회차/김나형/5_등산로조성.py
+++ 2회차/김나형/5_등산로조성.py
@@ -1,3 +1,10 @@
 import sys
 
 sys.stdin = open("_등산로조성.txt")
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, K  = map(int, input().split())
+
+    load = [list(map(int, input().split())) for _ in range(N)]
+    print(load)
