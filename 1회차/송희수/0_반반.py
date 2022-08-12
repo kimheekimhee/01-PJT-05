3a42ce4e4758e980d12ab0ba86fc075b31f0727f
star2871 <star2871@gmail.com>
2022-08-12 17:30:08 +0900

    1기_1회차_송희수 프로젝트05 모의고사

--- 1회차/송희수/0_반반.py
+++ 1회차/송희수/0_반반.py
@@ -1,3 +1,13 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+T = int(input())
+
+for tc in range(T):
+    word = list(input())
+    a = set(word)
+    if len(a) != 2:
+        print(f"#{tc+1} {'No'}")
+    else:
+        print(f"#{tc+1} {'Yes'}")
--- 1회차/송희수/1_모음이보이지않는사람.py
+++ 1회차/송희수/1_모음이보이지않는사람.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+unlook_word = ['a', 'e', 'i', 'o', 'u']
+for tc in range(1, T+1):
+    word =input()
+    word_list =[]
+    for n in word:
+        word_list.append(n)
+    for x in range(len(word_list)):
+        for y in unlook_word:
+            if y in word_list:
+                word_list.remove(y)
+    print('#%d' % tc,end=" ")
+    for k in word_list:
+        print(k, end ="")
+    print()
