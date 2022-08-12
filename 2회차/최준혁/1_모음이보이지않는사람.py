1ec992d1a3a5f9d9087c511713e14b55c2338e93
kimbap918 <fx887722@naver.com>
2022-08-12 14:55:26 +0900

    20220812 0~4 문제풀이 및 후기

--- 2회차/최준혁/1_모음이보이지않는사람.py
+++ 2회차/최준혁/1_모음이보이지않는사람.py
@@ -1,3 +1,19 @@
-import sys
+# import sys
 
-sys.stdin = open("_모음이보이지않는사람.txt")
+# sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+mo = ['a', 'e', 'i', 'o', 'u'] # 모음 리스트
+
+for t in range(1, T+1): # 1부터 테스트케이스 수+1 까지
+    a = [] # 모음이 제거된 문장을 받을 리스트
+    S = list(input()) # 입력
+
+    for i in range(len(S)): 
+        if S[i] in mo: # 모음이 포함되면
+            continue # 건너뜀
+        else: # 그외에
+            a.append(S[i]) # a에 담는다.
+
+    print("#{} {}".format(t, ''.join(a))) # 테스트케이스와 함께 a를 이어붙혀서 출력
+    
--- 2회차/최준혁/3_Flatten.py
+++ 2회차/최준혁/3_Flatten.py
@@ -1,3 +1,16 @@
-import sys
+# import sys
 
-sys.stdin = open("_Flatten.txt")
+# sys.stdin = open("_Flatten.txt")
+
+for t in range(1, 11): # 10번의 테스트 케이스
+    N = int(input()) # 덤프 횟수
+    box_hight = list(map(int, input().split())) # 상자의 높이값
+
+    for i in range(N): # 덤프횟수만큼
+        max_idx = box_hight.index(max(box_hight)) # 입력된 박스 높이의 최고점 인덱스
+        min_idx = box_hight.index(min(box_hight)) # 입력된 박스 높이의 최저점 인덱스
+        box_hight[max_idx] -= 1 # 박스 최고점에서 1개씩 차감
+        box_hight[min_idx] += 1 # 박스 최저점에서 1개씩 증가
+
+    # 테스트케이스와 최고점에서 최저점을 뺀 값(차이) 출력
+    print("#{} {}".format(t, max(box_hight)-min(box_hight)))
