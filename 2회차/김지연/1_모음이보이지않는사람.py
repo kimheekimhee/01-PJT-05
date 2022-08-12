94d323443dfb2d6ac1a06183c869575ef4d398f7
Jni97 <kjo5395@naver.com>
2022-08-12 17:29:35 +0900

    2022-08-12

--- 2회차/김지연/1_모음이보이지않는사람.py
+++ 2회차/김지연/1_모음이보이지않는사람.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+
+for tc in range(1, T+1): 
+    s = input()
+    result = ''
+
+    for char in s:
+        if char != 'a' and char != 'e' and char != 'i' and char != 'o' and char != 'u':
+            result += char
+    print('#{} {}'.format(tc, result))
