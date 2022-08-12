28cfda7ad8edb03bfacfbfbc36f61c8dec6d7e5d
jincde <asdzxc@kakao.com>
2022-08-12 13:13:03 +0900

    11586 풀이

--- 2회차/장영진/0_반반.py
+++ 2회차/장영진/0_반반.py
@@ -1,3 +1,21 @@
+# SWEA 11586. 반반
+
 import sys
 
 sys.stdin = open("_반반.txt")
+
+T = int(input())
+
+for i in range(1, T+1):
+    S = list(input())
+    S_dict = list(set(S))
+    flag = True
+
+    for j in S_dict:
+        if not S.count(j) == 2:
+            flag = False
+
+    if flag:
+        print(f'#{i} Yes')
+    else:
+        print(f'#{i} No')
