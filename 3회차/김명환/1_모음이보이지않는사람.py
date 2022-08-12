75954aa6a294269e65e2158d70ccb83cb1d98f65
myeonghwan57 <myeonghwan57@gmail.com>
2022-08-12 16:57:02 +0900

    3회차 김명환

--- 3회차/김명환/1_모음이보이지않는사람.py
+++ 3회차/김명환/1_모음이보이지않는사람.py
@@ -1,3 +1,12 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+vowels = ['a', 'e', 'i', 'o', 'u']
+T = int(input())
+for i in range(T):
+    word = input()
+    result = ''
+    for j in word:
+        if j not in vowels:
+            result += j
+    print(f'#{i+1} {result}')
--- 3회차/김명환/4_창용마을무리의개수.py
+++ 3회차/김명환/4_창용마을무리의개수.py
@@ -1,3 +1,29 @@
 import sys
 
 sys.stdin = open("_창용마을무리의개수.txt")
+def dfs (start):
+    stack = [start]
+    visited[start] = True
+    while stack:
+        now = stack.pop()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                stack.append(i)
+
+T = int(input())
+for i in range(T):
+    N, M = map(int,input().split())
+    graph = [[] for _ in range(N+1)]
+    for j in range(M):
+        v1, v2 = map(int,input().split())
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    visited = [False] * (N + 1)
+
+    cnt = 0
+    for j in range(1, N + 1):
+        if not visited[j]:
+            dfs(j)
+            cnt += 1
+    print(f'#{i+1} {cnt}')
