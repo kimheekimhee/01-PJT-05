3136f89630d6df741074059dcd7f84f707748e6e
rkskakdlfem3 <rkskakdlfem3@gmail.com>
2022-08-12 17:21:09 +0900

    0812 모의고사 문제풀이 이유주

--- 1회차/이유주/0_반반.py
+++ 1회차/이유주/0_반반.py
@@ -1,3 +1,11 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+TC =int(input())
+for i in range(1,TC+1):
+    S = input()
+    if len(set(S)) == 2:
+        print(f'#{i} Yes') 
+    else:
+        print(f'#{i} No')   
--- 1회차/이유주/2_퍼펙트셔플.py
+++ 1회차/이유주/2_퍼펙트셔플.py
@@ -1,3 +1,28 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+T = int(input()) #테스트케이스 수
+
+
+
+for i in range(1, T+1):
+    n = int(input()) #카드 수
+    divide = [0] * n
+    card = list(map(str, input().split()))
+   
+
+    #짝수일때
+    if n % 2 == 0:
+        for x in range(n//2):
+            divide[x*2] = card[x]
+            divide[(x*2)+1] = card[(n//2)+x]
+        
+    # #홀수일때
+    if n % 2 == 1:
+        for x in range(n//2+1):
+            divide[x*2] = card[x]
+        for x in range(n//2):
+            divide[x*2+1] = card[(n//2)+1+x]
+    print(f'#{i} {divide}')
+
