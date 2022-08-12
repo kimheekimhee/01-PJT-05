b86a50ba87f391ca7ef484060857c7c3ad224328
mgpo-98 <csi4613@naver.com>
2022-08-12 17:20:51 +0900

    0812mgpo

--- 2회차/조승윤/1_모음이보이지않는사람.py
+++ 2회차/조승윤/1_모음이보이지않는사람.py
@@ -1,3 +1,13 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+mo = ['a','e','i','o','u']
+
+t = int(input())
+for _ in range(1, t+1):
+    w = ''
+    s = input()
+    for i in s:
+        if i not in mo:
+            w += i
+    print(f'#{_} {w}')        
