a0630b26910abf1d01f4ced4443808ac631b95e7
kmckmc5587 <kmckmc5587@gmail.com>
2022-08-12 17:20:35 +0900

    1기_1회차_김민찬

--- 1회차/김민찬/2_퍼펙트셔플.py
+++ 1회차/김민찬/2_퍼펙트셔플.py
@@ -1,3 +1,28 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+T = int(input()) # 테스트 케이스의 수 T가 주어진다.
+for i in range(1, T + 1):
+    N = int(input()) # 자연수 N이 주어진다.
+    word = list(map(str, input().split()))
+    divide = N // 2 # 나뉘는 값 -> 2로 나눈다.
+    li = [] # 저장 공간
+    index = 1 # 위치 값
+
+    if N % 2 == 0: # N의 값이 짝수일 경우
+        for j in range(N):
+            if j < divide:
+                li.append(word[j])
+            else:
+                li.insert(index, word[j])
+                index += 2
+    else:
+        for j in range(N):
+            if j < divide + 1:
+                li.append(word[j])
+            else:
+                li.insert(index, word[j])
+                index += 2
+
+    print('#{}'.format(i), *li) # [] 제거하기 위해 * 사용
--- 1회차/김민찬/5_등산로조성.py
+++ 1회차/김민찬/5_등산로조성.py
@@ -1,3 +1,4 @@
 import sys
 
 sys.stdin = open("_등산로조성.txt")
+
