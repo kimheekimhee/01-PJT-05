cbcb1b11da7eebda2009fd3a375015294a9df574
dahlia <wlstnrdahlia@gmail.com>
2022-08-12 17:22:57 +0900

    Add : 모의고사 WEEK3 문제 풀이  업로드

--- 3회차/류진숙/2_퍼펙트셔플.py
+++ 3회차/류진숙/2_퍼펙트셔플.py
@@ -1,3 +1,37 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+# 반씩 우선 나눠주는 프로그램
+
+T = int(input())
+
+for n in range(1, T+1):
+    num = int(input())
+    cards_n = input().split()
+    if num % 2 == 0:
+        half = num // 2
+        list_1 = cards_n[:half]
+        list_2 = cards_n[half:]
+    else:
+        list_1 = cards_n[:half+1]
+        list_2 = cards_n[half+1:]
+
+    result_deck = [0] * num
+    for i in range(num):
+        if i % 2 == 0:
+            result_deck[i] = list_1.pop(0)
+        else:
+            result_deck[i] = list_2.pop(0)
+
+    print(f'#{n}', *result_deck)
+    
+    
+
+        
+
+    
+        
+
+
+        
