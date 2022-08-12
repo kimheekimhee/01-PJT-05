90682123da0738c9f466138f042992147e4b13f9
gownsuhun <rhqemd333@naver.com>
2022-08-12 17:23:12 +0900

    실습

--- 2회차/권수훈/1_모음이보이지않는사람.py
+++ 2회차/권수훈/1_모음이보이지않는사람.py
@@ -1,3 +1,24 @@
 import sys
 
+num = int(input())
+
+vowel = ["a","e","i","o","u"]
+
+
+count = 0
+for i in range(1,num+1):
+    letter = list(map(str,input()))
+    result = []
+    for j in letter:
+        if j not in vowel:
+            result.append(j)
+    print(f"#{i} {''.join(result)}")
+
+
+
+
+
+
+
+
 sys.stdin = open("_모음이보이지않는사람.txt")
