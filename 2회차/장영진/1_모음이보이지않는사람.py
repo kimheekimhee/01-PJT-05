08e3d597edac8345a7e89fa583a0a2ffc86428c8
jincde <asdzxc@kakao.com>
2022-08-12 13:22:31 +0900

    4406 풀이

--- 2회차/장영진/1_모음이보이지않는사람.py
+++ 2회차/장영진/1_모음이보이지않는사람.py
@@ -1,3 +1,17 @@
+# SWEA 4406. 모음이 보이지 않는 사람
+
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+for i in range(1, T+1):
+    S = input()
+
+    for j in vowels:
+        if j in S:
+            S = S.replace(j, '')
+
+    print(f'#{i} {S}')
