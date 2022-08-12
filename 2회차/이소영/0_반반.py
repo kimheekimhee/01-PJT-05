76d1b377ecef92ef951a41f8037e308b0ab0750e
soyolee-dev <soyolee.dev@gmail.com>
2022-08-12 16:48:47 +0900

    220812 이소영

--- 2회차/이소영/0_반반.py
+++ 2회차/이소영/0_반반.py
@@ -1,3 +1,12 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+t = int(input())
+
+for tc in range(t):
+    word = sorted(list(input()))
+    if word[0] == word[1] and word[2] == word[3] and word[1] != word[2]:
+        print(f'#{tc+1} Yes')
+    else:
+        print(f'#{tc+1} No')
--- 2회차/이소영/1_모음이보이지않는사람.py
+++ 2회차/이소영/1_모음이보이지않는사람.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+t = int(input())
+
+#word를 입력 받은 후 for문으로 돌면서 자음만 not_mo에 추가해 not_mo를 출력
+
+for t in range(1, t+1): 
+    word = input()
+    not_mo = ''
+    for i in word:
+        if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u':
+            not_mo += i
+
+    print(f'#{t} {not_mo}')
