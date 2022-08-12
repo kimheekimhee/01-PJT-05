b1c1df8c4176f26f627c1f47724961f67a214929
chiwoogood <chiwooooo@naver.com>
2022-08-12 16:33:32 +0900

    project5

--- 1회차/박치우/1_모음이보이지않는사람.py
+++ 1회차/박치우/1_모음이보이지않는사람.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+test_case = int(input())
+vowel = ['a','e','i','o','u']
+for i in range(test_case):
+    word = input()
+    lst = []
+    for i in word:
+        if i not in vowel:
+            lst.append(i)
+    lst = ''.join(lst)
+    print(lst)
+
