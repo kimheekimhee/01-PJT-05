66d237d3d20cd060d2f36c431dc78a5dcd47a2e3
kklee0930 <kklee0930@gmail.com>
2022-08-12 17:22:40 +0900

    220812 1기_3회차_이동희

--- 3회차/이동희/2_퍼펙트셔플.py
+++ 3회차/이동희/2_퍼펙트셔플.py
@@ -1,3 +1,40 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+
+from collections import deque
+T = int(input())
+
+for tc in range(1, T+1):
+    card_num = int(input())
+    cards_lst = input().split()
+    
+    med = card_num // 2   
+     
+    # 반올림이 기억이 안나서
+    if card_num % 2 == 0:
+        slicer = med
+    else:
+        slicer = med + 1
+    
+    lst_1 = deque(cards_lst[:slicer])
+    lst_2 = deque(cards_lst[slicer:])
+    
+    lst_1_size = len(lst_1)
+    lst_2_size = len(lst_2)
+
+    print(f'#{tc} ',end='')
+    count = 0
+    
+    while True:
+        if count < lst_1_size:
+            print(lst_1.popleft(), end=' ')
+            
+        if count < lst_2_size:
+            print(lst_2.popleft(), end=' ')
+        count += 1
+        
+        if count >= lst_1_size and count >= lst_2_size:
+            break
+    print()
