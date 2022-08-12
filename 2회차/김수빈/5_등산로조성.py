33346354d5e9c7bc132273afe5b5ed2a72d3c2b8
buzzzlight <pomodoro5292@gmail.com>
2022-08-12 17:33:45 +0900

    0812 모의고사

--- 2회차/김수빈/5_등산로조성.py
+++ 2회차/김수빈/5_등산로조성.py
@@ -1,3 +1,14 @@
 import sys
-
 sys.stdin = open("_등산로조성.txt")
+
+T = int(input())
+delta = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (0, -1), (-1, 1)]
+
+for i in range(1, T + 1):
+    N, K = map(int, input().split())
+    graph = []
+    for i in range(N):
+        graph.append(list(map(int, input().split())))
+    cnt = 0
+    print(max(max(graph)))
+
