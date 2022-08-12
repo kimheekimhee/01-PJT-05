be202bc97185e1410739a37de5b57739eebb48e5
choikeunyoung <ought119@gmail.com>
2022-08-12 17:20:39 +0900

    5번 제외 풀이

--- 2회차/최근영/1_모음이보이지않는사람.py
+++ 2회차/최근영/1_모음이보이지않는사람.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(sys.stdin.readline())
+
+vowel = ['a','e','i','o','u']
+
+for i in range(1,T+1):
+    word = sys.stdin.readline().strip()
+    for j in word:
+        if j in vowel:
+            word = word.replace(j,"")
+    print(f"#{i}",word)
---
+++ 2회차/최근영/test.py
@@ -0,0 +1,9 @@
+index_ = []
+a = [1, 2, 3]
+index_ = list(a)
+print(index_)
+a[0] = 3
+print(index_)
+b = [3, 3, 4]
+index_ = list(b)
+print(index_)
