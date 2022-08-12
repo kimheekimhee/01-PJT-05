72741143c1b543a2e7686b7d47ef6836fd6de3fa
byeongjin <qudwls7468@gmail.com>
2022-08-12 17:22:10 +0900

    220812 모의고사 풀이 및 후기 작성

--- 2회차/조병진/0_반반.py
+++ 2회차/조병진/0_반반.py
@@ -1,3 +1,17 @@
 import sys
+from tkinter import E
 
 sys.stdin = open("_반반.txt")
+
+# 문제풀이
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    alpha = input()
+    TWO_LIST = set(alpha)  # 중복 제거
+
+    if len(TWO_LIST) == 2:  # 리스트 요소 길이가 2라면 "Yes", 아니면 "No"
+        print(f'#{tc} {"Yes"}')
+    else:
+        print(f'#{tc} {"No"}')
--- 2회차/조병진/5_등산로조성.py
+++ 2회차/조병진/5_등산로조성.py
@@ -1,3 +1,37 @@
 import sys
 
 sys.stdin = open("_등산로조성.txt")
+
+
+def pprint(ls):
+    for row in ls:
+        print(row)
+
+# 문제풀이
+
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+
+    MAP = [list(map(int, input().split())) for _ in range(N)]  # 등산지도 리스트 생성
+    visited = [[0] * N for _ in range(N)]  # 방문처리할 이중 리스트 생성
+
+    dr = [0, 0, -1, 1]  # 4방위 델타 좌표
+    dy = [-1, 1, 0, 0]
+
+    high = 0
+    for i in range(N):  # 이중 반복문을 이용한 완전탐색
+        for j in range(N):
+            if high < MAP[i][j]:  # 가장 높은 수 구하기
+                high = MAP[i][j]
+
+    def DFS(r, c):
+        pass
+
+    for i in range(N):  # 이중 반복문을 이용한 완전탐색
+        for j in range(N):
+            if MAP[i][j] == high:  # 지도에서 좌표가 가장 큰 값과 동일하다면
+                visited[i][j] = 1  # 방문 처리
+                DFS(i, j)  # DFS 실행
