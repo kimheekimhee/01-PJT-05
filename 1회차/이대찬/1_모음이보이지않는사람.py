3783aad71f487c21975edafa3fa2be869fe2f191
djlfjfeq <leedc133@gmail.com>
2022-08-12 17:21:19 +0900

    1기 1회차 이대찬 PJT 05

--- 1회차/이대찬/1_모음이보이지않는사람.py
+++ 1회차/이대찬/1_모음이보이지않는사람.py
@@ -1,3 +1,14 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+N = int(input())
+
+alp = 'aeiou'
+for i in range (1,N+1):
+    string  = input()
+    for n in alp:
+        string = string.replace(n, '')
+    print(f'#{i} {string}')
+    
+    
--- 1회차/이대찬/2_퍼펙트셔플.py
+++ 1회차/이대찬/2_퍼펙트셔플.py
@@ -1,3 +1,33 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+N = int(input())
+
+for i in range (1,N+1):
+    result = []
+    count = int(input()) 
+    string = list(input().split())
+    if count %2 == 0:
+        a = int(count / 2 )
+    else:
+        a = int((count / 2 ) +1)
+        
+    for a_ in range(a):
+        result.append(string.pop(0))
+    index =1 
+    while(1):
+        result.insert(index,string.pop(0))
+        index +=2
+        if len(result) == count:
+            break
+    print(f'#{i} ',end=' ')
+    for k in result:
+        print(k,end=' ')
+    print()
+ 
+        
+    
+        
+        
+ 
