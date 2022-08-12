e73a2b08162cbe1c52e8afd1feb394e485584f10
hjkim1350 <hjkim1350@daum.net>
2022-08-12 17:20:41 +0900

    2022.08.12 모의고사 제출

--- 1회차/김현정/1_모음이보이지않는사람.py
+++ 1회차/김현정/1_모음이보이지않는사람.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+T = int(input())
+result = []
+
+for _ in range(1, T+1):
+    alpha = input()
+    alpha_re = ""
+
+    for i in alpha:
+        if i in 'aeiou':
+            alpha = alpha.replace(i, '')
+    
+    print(f"#{_}", alpha)
--- 1회차/김현정/4_창용마을무리의개수.py
+++ 1회차/김현정/4_창용마을무리의개수.py
@@ -1,3 +1,32 @@
+# 미해결문제
+
 import sys
 
 sys.stdin = open("_창용마을무리의개수.txt")
+
+T = int(input())
+
+for _ in range(1, T+1):
+    N, M = map(int, input().split())
+    town = []
+
+    for k in range(M):
+        man1, man2 = input().split()
+        
+        if k == 0:
+            town.append([man1, man2])
+        else:
+            for i in range(len(town)):
+                for j in range(len(town[i])):
+                    if (man1 in town[i] or man2 in town[i]):
+                        town[i].append(man1)
+                        town[i].append(man2)
+                        break
+                    else:
+                        town.append([man1, man2])
+                        break
+            
+            print(town)
+    print("최종")
+    print(town)
+    print("--------------------------------------------------------------")
