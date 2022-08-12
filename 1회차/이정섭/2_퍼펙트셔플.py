c18555c77a87684be4c05923e0e13f154d8f5f85
1_1_LJS <jeongseob.korea@gmail.com>
2022-08-12 17:22:04 +0900

    end of algorithm 0812

--- 1회차/이정섭/2_퍼펙트셔플.py
+++ 1회차/이정섭/2_퍼펙트셔플.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+t = int(input())
+
+for tc in range(1, t+1):
+
+    n = int(input())
+    card = list(input().split())
+    divided_1 = []
+    divided_2 = []
+    shuffled_deck = [] 
+
+    if n % 2 == 0:  # If it's an even numbers of cards, divide in half and append the cards in list divided_1 and 2 evenly
+                    # 
+        
