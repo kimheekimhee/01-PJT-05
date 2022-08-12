661ae30362ed2defc0690b3b7f51c468c7bb636d
kangdh208 <kangdh208@gmail.com>
2022-08-12 14:30:43 +0900

    0812 PJT 풀이

--- 1회차/강동현/0_반반.py
+++ 1회차/강동현/0_반반.py
@@ -1,3 +1,13 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+T = int(input())
+dic = {}
+for z in range(T):
+    line = list(input())
+    dic = sorted(line)
+    if dic[0]==dic[1] and dic[2]==dic[3] and dic[1]!=dic[2]:
+        print(f'#{z+1} Yes')
+    else:
+        print(f'#{z+1} No')
