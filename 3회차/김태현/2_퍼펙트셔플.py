5fe342d0e7e703cf445dd8c0b029215b36d7bd9c
unboxing96 <unboxing96@gmail.com>
2022-08-12 17:21:08 +0900

    PJT 05 1기 3회차 김태현 풀이

--- 3회차/김태현/2_퍼펙트셔플.py
+++ 3회차/김태현/2_퍼펙트셔플.py
@@ -1,3 +1,31 @@
+from collections import deque
 import sys
-
 sys.stdin = open("_퍼펙트셔플.txt")
+
+# 절반을 슬라이싱해서 큐 2개 생성
+# 각 큐에서 번갈아가며 결과 리스트에 추가
+
+T = int(input())
+
+for t in range(1, T+1):
+    result = []
+
+    length = int(input())
+    card_list = input().split()
+
+    first = deque()
+    second = deque()
+
+    for i in range(length):
+        if i < (length / 2):
+            first.append(card_list[i]) # deque(['A', 'B', 'C'])
+        else:
+            second.append(card_list[i]) # deque(['D', 'E', 'F'])
+    
+    for j in range(length):
+        if j % 2 == 0:
+            result.append(first.popleft())
+        else:
+            result.append(second.popleft())
+    
+    print(f"#{t}", *result)
