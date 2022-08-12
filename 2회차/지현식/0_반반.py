2e176ebae7a0c66130e09074e0afb2c6c4809074
HYUNSIK-JI <https://github.com/HYUNSIK-JI>
2022-08-13 00:57:08 +0900

    반례발견

--- 2회차/지현식/0_반반.py
+++ 2회차/지현식/0_반반.py
@@ -3,9 +3,20 @@ import sys
 sys.stdin = open("_반반.txt")
 
 for test_case in range(1, int(input()) + 1):
-    # 중복제거해서 문자가 두개라면 길이가 2개 임을 이용
-    s = set(input())
-    if len(s) == 2:
+    # 입력
+    s = input()
+    # 딕셔너리
+    dic = {}
+    # 중복 출력 방지을 위한 flag
+    check = False
+
+    for i in s:
+        dic[i] = dic.get(i, 0) + 1
+    # 딕셔너리의 value 값이 2가 아니면 해당 input 은 문자이다.
+    for v in dic.values():
+        if v != 2:
+            check = True
+            print(f"#{test_case} {'No'}")
+            break
+    if not check:
         print(f"#{test_case} {'Yes'}")
-    else:
-        print(f"#{test_case} {'No'}")
