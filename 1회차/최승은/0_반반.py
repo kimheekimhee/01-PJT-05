6d4cb5a2488d3fe1e0a7afd6322745e187b09fea
cse1940 <se1940@naver.com>
2022-08-12 14:32:58 +0900

    220812

--- 1회차/최승은/0_반반.py
+++ 1회차/최승은/0_반반.py
@@ -1,3 +1,16 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+t = int(input())
+
+for i in range(1, t+1):
+    s = list(input())
+    s.sort()
+    if s[0] == s[1] and s[2] == s[3] and s[1] != s[2]:
+        print(f"#{i} Yes")
+    else:
+        print(f"#{i} No")
+
+
+
--- 1회차/최승은/1_모음이보이지않는사람.py
+++ 1회차/최승은/1_모음이보이지않는사람.py
@@ -1,3 +1,19 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+t = int(input())
+
+for i in range(1, t+1):
+    word = input()
+    vowels = ["a", "e", "i", "o", "u"]
+    print(f"#{i}", end = " ")
+    for j in word:
+        if j not in vowels:
+            print(*j , end= "")
+    print()
+            
+
+        
+           
+        
