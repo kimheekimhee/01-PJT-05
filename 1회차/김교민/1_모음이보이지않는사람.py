4cc6d4d65beb13de3126bab3040daa77964ffd11
kyominkim1074 <kyominkim1074@gmail.com>
2022-08-12 17:23:10 +0900

    0812 모의고사 풀이

--- 1회차/김교민/1_모음이보이지않는사람.py
+++ 1회차/김교민/1_모음이보이지않는사람.py
@@ -1,3 +1,14 @@
+from dataclasses import replace
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+t=int(input())
+mo=['a','e','i','o','u']
+for case in range(1, t+1):
+    munja = input()
+    re = ''
+    for i in munja:
+        if i not in mo:
+            re += i
+    print(f'#{case} {re}')
--- 1회차/김교민/2_퍼펙트셔플.py
+++ 1회차/김교민/2_퍼펙트셔플.py
@@ -1,3 +1,22 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+t = int(input())
+
+for case in range(1, t+1):
+    n = int(input())
+    mun = list(input().split())
+    if len(mun)%2 == 0:
+        list_1=mun[:len(mun)//2]
+        list_2=mun[len(mun)//2:]
+    else:
+        list_1=mun[:len(mun)//2+1]
+        list_2=mun[len(mun)//2+1:]
+    answer=[]
+    while list_1 or list_2:
+        if list_1:
+            answer.append(list_1.pop(0))
+        if list_2:
+            answer.append(list_2.pop(0))
+    print(f'#{case}', *answer)
