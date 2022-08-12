536eaef12e7744deba739511997176c3908a7c06
min <smh92333@gmail.com>
2022-08-12 17:09:27 +0900

    0812

--- 2회차/장성민/3_Flatten.py
+++ 2회차/장성민/3_Flatten.py
@@ -1,3 +1,21 @@
 import sys
 
 sys.stdin = open("_Flatten.txt")
+
+T = 10
+
+result = []
+for tc in range(1, T + 1):
+    dump = int(input())
+    box = list(map(int, input().split()))
+
+    for i in range(dump):
+        box.sort()
+        box[0] += 1
+        box[-1] -= 1
+    
+    box.sort()
+    result.append(box[-1] - box[0])
+
+    print(f'#{tc}', result[0])
+    del result[0]
