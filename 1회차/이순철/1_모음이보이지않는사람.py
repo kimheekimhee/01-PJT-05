c4d145e892161be2def1550b668afa74bcae66ed
leesc8408 <leesc8408@gmail.com>
2022-08-12 17:21:58 +0900

    0812_프로젝트05_모의고사_이순철

--- 1회차/이순철/1_모음이보이지않는사람.py
+++ 1회차/이순철/1_모음이보이지않는사람.py
@@ -1,3 +1,12 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+t = int(input())
+for t_case in range(1, t + 1):
+
+    word = input()
+    for i in word:
+        if i in 'aeiou':
+            word = word.replace(i, '')
+    print(f'#{t_case} {word}')
--- 1회차/이순철/2_퍼펙트셔플.py
+++ 1회차/이순철/2_퍼펙트셔플.py
@@ -1,3 +1,30 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+t = int(input())
+for t_case in range(1, t + 1):
+
+    n = int(input())
+    cards = list(input().split())
+    # print(cards)
+
+    half = 0
+
+    if len(cards)%2 == 0:
+        half = len(cards)//2
+    else:
+        half = (len(cards)//2) + 1
+    a = [cards[i] for i in range(0, half)]
+    b = [cards[i] for i in range(half, len(cards))]
+    # print(a, b, sep='\n')
+    ab = [a, b]
+    # print(ab)
+    out = []
+    for x in range(len(a)):
+        for y in range(len(ab)):
+            try:
+                out.append(ab[y][x])
+            except:
+                break
+    print(f'#{t_case}', *out, sep=' ')
