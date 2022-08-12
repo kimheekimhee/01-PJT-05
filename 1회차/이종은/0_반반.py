4ffad3c2c5aed4d35e03a1f79e53de66fc37983b
tmsnvl95 <tmsnvl95@naver.com>
2022-08-12 17:23:12 +0900

    2번까지 완료

--- 1회차/이종은/0_반반.py
+++ 1회차/이종은/0_반반.py
@@ -1,3 +1,32 @@
 import sys
-
 sys.stdin = open("_반반.txt")
+
+
+t = int(input())
+
+for _ in range(t):
+    s = list(input())
+
+    
+    cnt = 0
+
+    for i in s:
+        
+        if s.count(i) == 2:
+            cnt = 1
+        else:
+            cnt = 0
+            break
+
+    if cnt == 1:
+        print(f'#{_+1} {"Yes"}')
+    else:
+        print(f'#{_+1} {"No"}')
+
+
+    
+
+
+
+
+# ['ABAB', 'CCDD', 'EFFE', 'EEEE', 'NONE']
--- 1회차/이종은/1_모음이보이지않는사람.py
+++ 1회차/이종은/1_모음이보이지않는사람.py
@@ -1,3 +1,26 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+# mo = ['a', 'e', 'i', 'o', 'u']
+
+mo = "aeiou"
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+
+    for i in s:
+        if i in mo:
+            s = s.replace(i, '')
+
+    print(f'#{_+1} {s}')
+
+
+            
+    
+    
+
+
+    
