d4a09df5f79320f069cd3b38b88cae92be228b5f
rohsnaghyun <rohwinter@gmail.com>
2022-08-12 17:21:14 +0900

    0812모의고사

--- 3회차/노상현/0_반반.py
+++ 3회차/노상현/0_반반.py
@@ -1,3 +1,8 @@
 import sys
 
 sys.stdin = open("_반반.txt")
+
+
+for _ in range(int(input())):
+    s = sorted(list(input()))
+    print("#{} {}".format(_+1, 'Yes' if s[0]==s[1] and s[2]==s[3] and s[1] != s[2] else 'No'))
--- 3회차/노상현/2_퍼펙트셔플.py
+++ 3회차/노상현/2_퍼펙트셔플.py
@@ -1,3 +1,7 @@
 import sys
 
 sys.stdin = open("_퍼펙트셔플.txt")
+
+T = int(input())
+for tc in range(1, T + 1):
+    
