e74b6ad4bb259c4bc31dd49d80c92d136d5dde7d
oiosu <kdlatnrud123@naver.com>
2022-08-12 17:28:01 +0900

    PJT_05

--- 1회차/임수경/1_모음이보이지않는사람.py
+++ 1회차/임수경/1_모음이보이지않는사람.py
@@ -1,3 +1,29 @@
 import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
+
+# t = int(input())
+
+# for tc in range(1, t+1):
+#     data = input()
+#     result = " "
+#     for i in range(len(data)):
+#         if data[1] in ['a', 'e', 'i', 'o', 'u']:
+#             continue
+#         result += data[i]
+
+#     print('#%d %s' % (tc, result))
+
+
+T = int(input())
+ 
+for tc in range(1,1+T):
+    word = str(input())
+    my_word = ''
+ 
+    for q in word:
+        
+        if q != 'a' and q != 'e' and q != 'i' and q != 'o' and q != 'u':
+            my_word += q
+ 
+    print('#{} {}'.format(tc,my_word))
