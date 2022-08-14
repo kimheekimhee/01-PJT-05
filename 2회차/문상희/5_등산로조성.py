0a4358a8978260929d8d280cd5e0ae3719efa46a
MoonSanghee <psycho13th@gmail.com>
2022-08-14 15:41:32 +0900

    3주차 모의고사 다른사람 코드 참고 미해결 문제 풀이 추가

--- 2회차/문상희/5_등산로조성.py
+++ 2회차/문상희/5_등산로조성.py
@@ -3,83 +3,110 @@ import sys
 sys.stdin = open("_등산로조성.txt")
 from collections import deque
 
-test_case = int(input())
-for test in range(1, test_case + 1):
-    size, depth = list(map(int, input().split()))
-    maps = []
-    check = []
-    highest = 0
-    h_p = []
-    for i in range(size):
-        line = list(map(int, input().split()))
-        maps.append(line)
-        hang = []
-        for j in range(size):
-            hang.append(0)
-            if maps[i][j] > highest:
-                highest = maps[i][j]
-                h_p = []
-                h_p.append([i, j])
-            elif maps[i][j] == highest:
-                h_p.append([i, j])
-        check.append(hang)
-    for i in h_p:
-        check[i[0]][i[1]] = 1
-
-    di = [1, -1, 0, 0]
-    dj = [0, 0, -1, 1]
-
-    def dfs(x, y, cut):
-        stack = list()
-        stack.append((x, y))
-        maximum = 0
-        if cut == 0:
-            maps[x][y] -= depth
-        while stack:
-            a, b = stack.pop()
-            for i in range(4):
-                ni = a + di[i]
-                nj = b + dj[i]
-                if ni in range(size) and nj in range(size):
-                    if maps[ni][nj] < maps[a][b]:
-                        if check[ni][nj] <= check[a][b]:
-                            stack.append((ni, nj))
-                            check[ni][nj] = check[a][b] + 1
-                            if check[ni][nj] > maximum:
-                                maximum = check[ni][nj]
-
-                    elif maps[ni][nj] - depth < maps[a][b]:
-                        if cut == 1:
-                            cut -= 1
-                            stack2 = list()
-                            stack2.append((ni, nj))
-                            maps[ni][nj] -= depth
-                            while stack2:
-                                a2, b2 = stack2.pop()
-                                for j in range(4):
-                                    ni2 = a2 + di[j]
-                                    nj2 = b2 + dj[j]
-                                    if ni2 in range(size) and nj2 in range(size):
-                                        if maps[ni2][nj2] < maps[a2][b2]:
-                                            if check[ni2][nj2] <= check[a2][b2]:
-                                                stack2.append((ni2, nj2))
-                                                check[ni2][nj2] = check[a2][b2] + 1
-                                                if check[ni2][nj2] > maximum:
-                                                    maximum = check[ni2][nj2]
-                            maps[ni][nj] += depth
-        return maximum
+# 지현식님 코드 참고 작성 코드
+# test_case = int(input())
+# for test in range(1, test_case + 1):
+#     size, depth = list(map(int, input().split()))
+#     maps = []
+#     highest = 0
+#     for i in range(size):
+#         line = list(map(int, input().split()))
+#         num = max(line)
+#         if num > highest:
+#             highest = num
+#         maps.append(line)
     
-    long = []
-    for p in h_p:
-        i, j = p
-        if check[i][j] == 1:
-            x = dfs(i, j, 1)
-            long.append(x)
-            for i2 in range(size):
-                for j2 in range(size):
-                    if maps[i2][j2] == highest:
-                        check[i2][j2] = 1
-                    else:
-                        check[i2][j2] = 0
-    print(long)
-    # print(f'#{test} {max(long)}')
+#     check =[]
+#     for i in range(size):
+#         for j in range(size):
+#             if maps[i][j] == highest:
+#                 check.append([i, j])
+
+#     di = [1, -1, 0, 0]
+#     dj = [0, 0, -1, 1]
+
+#     def bfs(a, b):
+#         q = deque()
+#         q.append((a, b, 1))
+#         length = 0
+#         while q:
+#             i, j, cnt = q.popleft()
+#             length = max(length, cnt)
+#             for x in range(4):
+#                 ni = i + di[x]
+#                 nj = j + dj[x]
+
+#                 if ni in range(size) and nj in range(size):
+#                     if maps[ni][nj] < maps[i][j]:
+#                         q.append((ni, nj, cnt + 1))
+
+#         return length
+
+#     def fix(size, depth):
+#         route = []
+#         for i in range(size):
+#             for j in range(size):
+#                 for fix in range(1, depth + 1):
+#                     maps[i][j] -= fix
+#                     for a, b in check:
+#                         route.append(bfs(a, b))
+#                     maps[i][j] += fix
+#         return max(route)
+    
+#     print(f'#{test} {fix(size, depth)}')
+
+
+
+                
+
+
+# 구글링 통해 나온 재귀를 활용하는 방법 적용 시도 실패 코드
+# test_case = int(input())
+# for test in range(1, test_case + 1):
+#     size, depth = list(map(int, input().split()))
+#     maps = []
+#     highest = 0
+#     for i in range(size):
+#         line = list(map(int, input().split()))
+#         num = max(line)
+#         if num > highest:
+#             highest = num
+#         maps.append(line)
+    
+#     check =[]
+#     for i in range(size):
+#         for j in range(size):
+#             if maps[i][j] == highest:
+#                 check.append([i, j])
+    
+#     di = [1, -1, 0, 0]
+#     dj = [0, 0, 1, -1]
+    
+#     def bfs(a, b, n, bool):
+#         q = deque()
+#         q.append((a, b, n, bool))
+#         length = 0
+#         while q:
+#             x, y, cnt, cut = q.popleft()
+#             if length < cnt:
+#                 length = cnt
+#             for i in range(4):
+#                 ni = x + di[i]
+#                 nj = y + dj[i]
+#                 if ni in range(size) and nj in range(size):
+#                     if maps[ni][nj] < maps[x][y]:
+#                         q.append((ni, nj, cnt+1, True))
+#                     elif cut == True and (maps[ni][nj] - depth) < maps[x][y]:
+#                         before = maps[ni][nj]
+#                         maps[ni][nj] = maps[x][y] - 1
+#                         result = bfs(ni, nj, cnt+1, False)
+#                         maps[ni][nj] = before
+#                         if result > length:
+#                             length = result
+
+#         return length
+
+#     route = []
+#     for i in check:
+#         route.append(bfs(i[0], i[1], 1, True))
+#     print(route)
