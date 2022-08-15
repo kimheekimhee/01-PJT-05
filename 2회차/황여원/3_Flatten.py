887c8f878d94b0da769adbf328d0adcc06f1150f
황여원 <yeooness@naver.com>
2022-08-15 16:47:57 +0900

    2회차 황여원 모의고사

--- 2회차/황여원/3_Flatten.py
+++ 2회차/황여원/3_Flatten.py
@@ -10,8 +10,19 @@ for tc in range(1,T+1) :
     box = list(map(int,input().split()))
 
     for i in range(dump) : 
-        # 최댓값 최솟값 구해주고,,그 값이 어디있는지 찾아서...?
+        # 최댓값 최솟값 구해주고
         max_ = max(box)
         min_ = min(box)
+
+        # 최대,최소 인덱스를 찾아서
+        index_max = box.index(max_)
+        index_min = box.index(min_)
         
-        
+        # 내려주고 올려주기 
+        box[index_max] -= 1
+        box[index_min] += 1
+
+        # 최댓값과 최솟값 차이 
+        result = max(box)-min(box)
+
+    print(f'#{tc} {result}')
