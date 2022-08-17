49f3822403b4b36c290763df2d577e6c6b71f3ef
tmsnvl95 <tmsnvl95@naver.com>
2022-08-17 09:09:40 +0900

    4update0816

--- 1회차/이종은/0_반반.py
+++ 1회차/이종은/0_반반.py
@@ -10,13 +10,13 @@ for _ in range(t):
     
     cnt = 0
 
-    for i in s:
+    for i in s: # s에 문자열을 리스트로 받음
         
-        if s.count(i) == 2:
-            cnt = 1
-        else:
-            cnt = 0
-            break
+        if s.count(i) == 2: # s리스트에 s요소의 갯수를 확인하여 2개이면 cnt에 1을 추가
+            cnt = 1 
+        else: # 아니면 
+            cnt = 0 # cnt를 0으로 초기화
+            break 
 
     if cnt == 1:
         print(f'#{_+1} {"Yes"}')
