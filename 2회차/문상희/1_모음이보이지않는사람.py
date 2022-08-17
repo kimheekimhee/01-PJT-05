2580295d13297a58715782bfb1a7071c3f9d7a1f
MoonSanghee <psycho13th@gmail.com>
2022-08-17 18:11:16 +0900

    모음이 보이지 않는 사람 replace 이용 코드 추가

--- 2회차/문상희/1_모음이보이지않는사람.py
+++ 2회차/문상희/1_모음이보이지않는사람.py
@@ -2,13 +2,24 @@ import sys
 
 sys.stdin = open("_모음이보이지않는사람.txt")
 
-test_case = int(input())
-for test in range(1, test_case + 1):
+# 리뷰 적용 코드
+test = int(input())
+for t in range(1, test + 1):
     word = input()
     check = 'aeiou'
-    result = ''
-    for i in word:
-        if i not in check:
-            result += i
-    print(f'#{test} {result}')
+    for i in check:
+        word = word.replace(i, '')
+    print(word)
+
+
+
+# test_case = int(input())
+# for test in range(1, test_case + 1):
+#     word = input()
+#     check = 'aeiou'
+#     result = ''
+#     for i in word:
+#         if i not in check:
+#             result += i
+#     print(f'#{test} {result}')
     # 주어진 문자열을 순회하며 모음 aeiou를 제외한 문자들을 포함한 새로운 문자열을 만들어 출력합니다.
